21d292c445dabe3a1663afc0ed301687ec547f7f
Unknown <crazdave@gmail.com>
2017-10-08 01:15:53 -0700

    Added server functions for creating/joining group

--- server.h
+++ server.h
@@ -16,6 +16,7 @@ public:
     bool create_account(QString& username, QString& password, QString& email);
     // Group Functions
     bool create_group(QString& group_name, QString& group_id);
+    bool join_group(QString& group_id);
 
     // Access
     QString get_username();
@@ -36,6 +37,11 @@ public slots:
 private:
     QTcpSocket* my_socket;
     QString username;
+    bool success_flag; // ***Always set this to 0 before checking, ONLY to be set to 1 by the read_socket_send_signal() function.
+    bool fail_flag;    // ***Always set this to 0 before checking, ONLY to be set to 1 by the read_socket_send_signal() function.
+    QString success_message; // ***Always set this to nullptr before checking, ONLY to be set by the read_socket_send_signal() function.
+
+    QString message_length_string(QString& input_string);
 };
 
 #endif // SERVER_H
