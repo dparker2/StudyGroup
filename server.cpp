fba0af2af4525176fee9c5391e6ef418fcc8d15d
Unknown <crazdave@gmail.com>
2017-10-11 10:42:12 -0700

    Fixed the QString and QByteArray errors

--- server.cpp
+++ server.cpp
@@ -170,13 +170,13 @@ void server::read_socket_send_signal()
  *
  */
 
-QString server::format_socket_request(const QString &request_code, QString request_arg)
+QByteArray server::format_socket_request(const QString &request_code, QString request_arg)
 {
     QString full_request = request_code + request_arg;
     QString request_length = QString::number(full_request.size());
     full_request = full_request.prepend(request_length.rightJustified(5, '0', true));
     qDebug() << "Sending: " << full_request;
-    return full_request;
+    return full_request.toLatin1();
 }
 
 bool server::read_socket_helper(QString& out_message)
