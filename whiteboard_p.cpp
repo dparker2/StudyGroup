b39a4b6cdbbe515d777e4617e9f4d6a43a35143c
Unknown <crazdave@gmail.com>
2017-11-16 09:39:48 -0800

    hotfix

--- whiteboard_p.cpp
+++ whiteboard_p.cpp
@@ -44,7 +44,7 @@ QByteArray* my_whiteboard::get_whiteboard()
 
 void my_whiteboard::update_whiteboard(QByteArray* wb_data)
 {
-    image = QImage::fromData(wb_data, "PNG"); // reads bytes into an image in PNG format
+    image = QImage::fromData(*wb_data, "PNG"); // reads bytes into an image in PNG format
     update(image.rect());
 }
 
