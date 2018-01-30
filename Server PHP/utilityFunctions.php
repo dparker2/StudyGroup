c140d96d34a3d3b3478509826bb22b9019974273
Fuller Le <fullernle@mail.fresnostate.edu>
2018-01-30 11:36:39 -0800

    Updated User Class and Added get rows function
    need testing

--- Server PHP/utilityFunctions.php
+++ Server PHP/utilityFunctions.php
@@ -26,25 +26,61 @@ function disconnect($currConnect) {
   }
 }
 
+function getNumRows($connection, $query) {
+  $queryResult = mysqli_query($connection, $query);
+  $numRows = $resultGroups->num_rows;
+  return $numRows;
+}
+
+function getObjString($connection, $query) {
+  $queryResult = mysqli_query($connection, $query);
+  $object = $queryResult->fetch_object();
+  return $object;
+}
+
+function sendMessage($message, $socket) {
+  $messageSize = str_pad((string)strlen($message), 5, "0", STR_PAD_LEFT);
+  fwrite($socket, "{$messageSize}{$message}");
+}
+
 class user {
   function __construct() {
-    echo "Constructing";
+    echo "Constructing user class\n";
   }
   function __destruct() {
-    echo "Destructing";
+    echo "Destructing user class\n";
   }
-  function getName() {
-    return $username;
-  }
-  function getIP() {
-    return $ip;
-  }
-  function getSocket() {
-    return $socket;
-  }
+
   var $username;
   var $ip;
   var $socket;
   var $email;
+
+  function getName() {
+    return $this->username;
+  }
+  function getIP() {
+    return $this->ip;
+  }
+  function getSocket() {
+    return $this->socket;
+  }
+  function getEmail() {
+    return $this->email;
+  }
+
+  function setName($Name) {
+    $this->username = $Name;
+  }
+  function setIP($ipAddress) {
+    $this->ip = $ipAddress;
+  }
+  function setSocket($sock) {
+    $this->socket = $sock;
+  }
+  function setEmail($usrEmail) {
+    $this->email = $usrEmail;
+  }
+
 }
 ?>
