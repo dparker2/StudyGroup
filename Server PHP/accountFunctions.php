cc1b1600457abe78b1138b7f8280a25712322886
Unknown <garret95@mail.fresnostate.edu>
2018-03-01 11:42:12 -0800

    update to remote

--- Server PHP/accountFunctions.php
+++ Server PHP/accountFunctions.php
@@ -2,6 +2,7 @@
 include_once 'db_credentials.php';
 include_once 'sendEmail.php';
 include_once 'utilityFunctions.php';
+include_once 'timeout.php';
 /* Group Functions
   function createAccount($email, $username, $password, $sock);
   function loginAccount($username, $password, $sock);
@@ -54,6 +55,7 @@ function createAccount($email, $username, $password, $sock) {
 
 function loginAccount($username, $password, $sock) {
   $connection = connectAccount();
+  global $a;
   $bool_check = false;
   $check_password = "SELECT Pass FROM UserInfo WHERE Username = '$username'";
   $check_username = "SELECT Username FROM UserInfo WHERE Username = '$username'";
@@ -61,8 +63,13 @@ function loginAccount($username, $password, $sock) {
   $change_online = "UPDATE UserInfo SET UserStatus='Online' WHERE Username = '$username'";
   //Checks if username exists before attempting to login, will return error otherwise.
   if (($username_exists = checkExists($connection, $check_username)) > 0) {
+	  if ($a>=5) {
+	    loginTimeout();
+		disconnect($connection);
+	    return $bool_check;
+	  }
       $checkPass = getObjString($connection, $check_password)->Pass;
-      if (password_verify($password, $checkPass)) {
+      else if (password_verify($password, $checkPass)) {
         $resultEmail = getObjString($connection, $check_email)->Email;
         $message = "SUCC{$resultEmail}"; //Successful if matches and writes back email belonging to user for UI
         sendMessage($message, $sock);
@@ -71,6 +78,7 @@ function loginAccount($username, $password, $sock) {
       } //Closes password check.
       else{
         $message = "FAILPassword incorrect, please try again.";
+		loginTimeout();
         sendMessage($message, $sock);
       }
   }
@@ -116,6 +124,7 @@ function changePassword($username, $password, $sock) {
 // unfinised account recovery using email method. outdated, unused, unloved
 function recoverAccount($email, $password, $sock) {
   $connection = connectAccount();
+
   //check if email exists before attempting to send recovery email, will return error otherwise.
   $check_email = "SELECT Email FROM UserInfo WHERE Email = '$email'";
   $change_password = "UPDATE UserInfo SET Pass= '$newPass' WHERE Email = '$email'";
@@ -143,6 +152,7 @@ function recoverAccount($email, $password, $sock) {
 // takes users email, returns users username.
 function rememberUsername ($email, $sock) {
   $connection = connectAccount();
+
   $find_user = "SELECT Username FROM UserInfo WHERE Email = '$email'";  //finds a username tied to a email
   if(checkExists($connection, $find_user) > 0) {
     $returnUser = getObjString($connection, $find_user)->Username;
@@ -156,7 +166,6 @@ function rememberUsername ($email, $sock) {
   }
   disconnect($connection);
 }
-
 
 // recovery option for remembering a password, sends a recovery email
 function rememberPassword ($username, $email, $sock) {
---
+++ Server PHP/timeout.php
@@ -0,0 +1,75 @@
+<?php
+
+include_once 'utilityFunctions.php';
+
+$a = 0;
+$s = 0;
+
+function microtime_float() {
+    list($usec, $sec) = explode(" ", microtime());
+    return ((float)$usec + (float)$sec);
+}
+
+function loginTimeout(){
+	global $a;
+	$time_start = microtime_float();
+	static $t = $time_start; // need time_start to be static, cant directly declare it as static.
+	$a++;
+	if (a==4){ // 4 failed attempts
+		$time_end = microtime_float();
+		$time = $time_end - $t;
+		if ($time > 60){ // 60 seconds since first fail, restart timeout
+			$t = microtime_float();
+			$a = 1;
+		}
+		else {
+			$message = "4 login fails";
+			sendMessage($message, $sock);
+		}
+	}
+	else if (a>=5){ // 5+ failed attempts
+		$time_end = microtime_float();
+		$time = $time_end - $t;
+		if ($time > 60){ // 60 seconds since first fail, restart timeout
+			$t = microtime_float();
+			$a = 1;
+		}
+		else {
+			$message = "5+ login fails";
+			sendMessage($message, $sock);
+		}
+	}
+}
+
+function securityTimeout(){
+	global $s;
+	$time_start = microtime_float();
+	static $t = $time_start;
+	$s++;
+	if (a==4){ // 4 failed attempts
+		$time_end = microtime_float();
+		$time = $time_end - $t;
+		if ($time > 60){ // 60 seconds since first fail, restart timeout
+			$t = microtime_float();
+			$s = 1;
+		}
+		else {
+			$message = "4 security fails";
+			sendMessage($message, $sock);
+		}
+	}
+	else if (a>=5){ // 5+ failed attempts
+		$time_end = microtime_float();
+		$time = $time_end - $t;
+		if ($time > 60){ // 60 seconds since first fail, restart timeout
+			$t = microtime_float();
+			$s = 1;
+		}
+		else {
+			$message = "5+ security fails";
+			sendMessage($message, $sock);
+		}
+	}
+}
+
+?>
