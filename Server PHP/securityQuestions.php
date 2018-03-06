52838f0b5a591cbf4688668453e51a14c4836917
Unknown <juanmejia10@mail.fresnostate.edu>
2018-03-06 15:20:05 -0800

    tons of debugs for setting q
    added lots of echos to see how variables act and also added mysql error report

--- Server PHP/securityQuestions.php
+++ Server PHP/securityQuestions.php
@@ -8,10 +8,13 @@ include_once 'sendEmail.php';
 function reqSecQuest($user, $sock) {
   $connection = connectAccount();
   $username = $user;
-  echo "the username input to the function is: $username";
+  echo "the username input to the function is: $username\n\n";
   $query1 = "SELECT SQ1 FROM UserInfo WHERE Username = '$username'";
+  echo "This is question1 query: $query1";
   $query2 = "SELECT SQ2 FROM UserInfo WHERE Username = '$username'";
+  echo "This is question2 query: $query2";
   $query3 = "SELECT SQ3 FROM UserInfo WHERE Username = '$username'";
+  echo "This is question3 query: $query3";
 
   /* not working for now, think it's a getobj string error
   $q1 = mysqli_query($connection, $query1);
@@ -23,11 +26,11 @@ function reqSecQuest($user, $sock) {
   */
 
   $q1 = getObjString($connection, $query1)->SQ1;
-  echo "This is q1: " . "$q1\n";
+  echo "This is q1 as string: " . "$q1\n";
   $q2 = getObjString($connection, $query2)->SQ2;
-  //echo "This is q2: {$q2}\n\n";
+  echo "This is q2 as object? : " . "{$q2}\n\n";
   $q3 = getObjString($connection, $query3)->SQ3;
-  //echo "This is q3: {$q3}\n\n";
+  echo "This is q3 as object? : " . "{$q3}\n\n";
 
   $message = "REQQ{$q1} {$q2} {$q3}";
   sendMessage($message, $sock);
@@ -55,13 +58,24 @@ function reqSecAns($user, $sock) {
 function setSecQuest($user, $q1, $q2, $q3, $sock) {
   $connection = connectAccount();
   $username = $user;
+  echo "This is q1 as received by setSecQuest: $q1\n\n";
+  echo "This is q2 as received by setSecQuest: $q2\n\n";
+  echo "This is q3 as received by setSecQuest: $q3\n\n";
   $escQ1 = mysqli_real_escape_string($connection, $q1);
   $escQ2 = mysqli_real_escape_string($connection, $q2);
   $escQ3 = mysqli_real_escape_string($connection, $q3);
+  echo "This is escq1: $escQ1\n\n";
+  echo "This is escq2: $escQ2\n\n";
+  echo "This is escq3: $escQ3\n\n";
   $query = "UPDATE UserInfo SET SQ1='$escQ1', SET SQ2='$escQ2', SET SQ3='$escQ3' WHERE Username = '$username'";
+  echo "Here is the setSecQuest query: $query\n\n";
   if (!mysqli_query($connection, $query)) {
     $message = "FAILThere was an issue setting the security questions.\n\n";
+    echo("Error description: " . mysqli_error($connection));
     sendMessage($message, $sock);
+  }
+  else {
+
   }
 
 }
